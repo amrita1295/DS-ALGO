a091586a2fb049fe2ab9e43f972dde8362c20ab3
Amrita Singh <56605853+amrita1295@users.noreply.github.com>
2021-06-19 19:04:57 +0530

    reverse an array of numbers

--- arrays/j_babbar_1.java
+++ arrays/j_babbar_1.java
@@ -1,1 +1,24 @@
 
+import java.io.*;
+import java.util.*;
+class j_babbar_1
+{
+	public static void main(String[] args)throws IOException {
+		//BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the size");
+		int n=sc.nextInt();
+		int numbers[]=new int[n];
+		System.out.println("Enter the "+n+" "+"numbers : ");
+		for(int i=0;i<n;i++)
+		{
+			numbers[i]=sc.nextInt();
+		}
+
+		for(int j=n-1;j>=0;j--)
+		{
+			System.out.print(numbers[j]+" ");
+		}
+		
+	}
+}
